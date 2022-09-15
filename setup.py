79a2083e7b5640a149879e24839c2eabbd904d4c
automatethem <108699325+automatethem@users.noreply.github.com>
2022-09-15 11:44:56 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pytorch_lightning_utils',
+    version='0.0.1',
+    description='pytorch_lightning_utils',
+    url='https://github.com/automatethem/pytorch_lightning_utils',
+    author='Sang Ki Kwon',
+    author_email='automatethem@gmail.com',
+    license='Sang Ki Kwon',
+    packages=['pytorch_lightning_utils'],
+    zip_safe=False,
+    install_requires=setup_tools._load_requirements(_PATH_ROOT)
+)
