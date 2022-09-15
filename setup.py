e14d6f393e6124d53ee439ef22f7a2ef136c1f43
automatethem <108699325+automatethem@users.noreply.github.com>
2022-09-15 12:13:07 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
-rom setuptools import setup
-
-VERSION = "0.1"
-
+from distutils.core import setup
 setup(
-    name="datasette-expose-some-environment-variables",
-    description="Expose environment variables in Datasette at /-/env",
-    author="Simon Willison",
-    license="Apache License, Version 2.0",
-    version=VERSION,
-    py_modules=["datasette_expose_some_environment_variables"],
-    entry_points={
-        "datasette": [
-            "expose_some_environment_variables = datasette_expose_some_environment_variables"
-        ]
-    },
-    install_requires=["datasette"],
+    name='mydates',
+    version='0.0.1',
+    author='X.X',
+    author_email='xx@example.com',
+    url='www.example.com',
+    license='LICENSE',
+    packages=['mydates'],
+    description='An example of building Python package.',
+    install_requires=[
+    #'python>=3.6.0',
+    #'pandas>=0.10.0'
+    ]
 )
