034ef8900e4e029dc600f7f535c23d47dadcffc3
automatethem <108699325+automatethem@users.noreply.github.com>
2022-09-15 12:10:20 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,18 @@
-from setuptools import setup, find_packages
+rom setuptools import setup
 
-with open('requirements.txt') as f:
-requirements = f.read().splitlines()
+VERSION = "0.1"
 
-with open("README.md", "r") as fh:
-long_description = fh.read()
-
-setuptools.setup(
-name="simple_scheduler", # 이 패키지를 설치/삭제할 때 사용할 이름을 의미한다. 이 이름과 import할 때 쓰이는 이름은 다르다.
-version="0.0.1",
-author="Donghyun Kwak",
-author_email="donghyun.kwak@naver.com",
-description="A simplest scheduler for automated data preprocessing and nlu model training.",
-long_description=long_description,
-long_description_content_type="text/markdown",
-url="https://github.com/dhkwak/simple_scheduler",
-packages=setuptools.find_packages(),
-classifiers=[
-"Programming Language :: Python :: 3",
-"License :: OSI Approved :: MIT License",
-"Operating System :: OS Independent",
-],
-python_requires='>=3.6',
-install_requires=requirements,
+setup(
+    name="datasette-expose-some-environment-variables",
+    description="Expose environment variables in Datasette at /-/env",
+    author="Simon Willison",
+    license="Apache License, Version 2.0",
+    version=VERSION,
+    py_modules=["datasette_expose_some_environment_variables"],
+    entry_points={
+        "datasette": [
+            "expose_some_environment_variables = datasette_expose_some_environment_variables"
+        ]
+    },
+    install_requires=["datasette"],
 )
