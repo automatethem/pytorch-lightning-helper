c8967ff80a268d5e1205a321092bc8d69a230695
automatethem <108699325+automatethem@users.noreply.github.com>
2022-09-17 22:20:55 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 import os
-from setuptools import *
+import setuptools
 
 def requirements():
     with open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), encoding='utf-8') as f:
         return f.read().splitlines()
 
-LONG_DESC = """
-This is pytorch lightning helper
-"""
-
-setup(name='pytorch-lightning-helper',
-	  version='0.0.5',
-	  description='Pytorch lightning helper',
-	  long_description=LONG_DESC,
-	  author='Sang Ki Kwon',
-	  url='https://github.com/automatethem/pytorch-lightning-helper',
-	  install_requires=requirements(),
-	  install_requires=[],
-	  author_email='automatethem@gmail.com',
-	  license='MIT',
-	  packages=find_packages(),
-	  zip_safe=False)
+setuptools.setup(
+	name='pytorch-lightning-helper',
+	version='0.0.6',
+	description='Pytorch lightning helper',
+	long_description=open('README.md').read(),
+	author='Sang Ki Kwon',
+	url='https://github.com/automatethem/pytorch-lightning-helper',
+	packages=setuptools.find_packages(),
+	install_requires=requirements(),
+	author_email='automatethem@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False
+)
